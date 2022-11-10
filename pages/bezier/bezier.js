9bd788635ace9015296e10022ea3206fce2607d2
Zach Stone <zach@zerovolts.com>
2022-11-09 23:41:51 -0500

    Add App abstraction

--- pages/bezier/bezier.js
+++ pages/bezier/bezier.js
@@ -1,82 +1,88 @@
+import { App } from "/shared/app.js"
 import { createShaderProgram, getAttributeLocations, getUniformLocations } from "/shared/graphics.js";
 import * as Vec2 from "/shared/vec2.js";
 import * as Vec3 from "/shared/vec3.js";
 
 // Initiate the fetch first to reduce perceived loading.
-const shaderSources = Promise.all([
+let shaderSources = Promise.all([
     fetch("/pages/bezier/bezier.vert").then(res => res.text()),
     fetch("/pages/bezier/bezier.frag").then(res => res.text()),
 ]);
 
-document.addEventListener("DOMContentLoaded", () => {
-    /** @type HTMLCanvasElement */
-    const canvas = document.getElementById("main-canvas");
-    const gl = canvas.getContext("webgl2");
-    if (gl === null) return;
+document.addEventListener("DOMContentLoaded", async () => {
+    shaderSources = await shaderSources;
 
-    render(gl, [canvas.clientWidth, canvas.clientHeight]);
+    const canvas = document.getElementById("main-canvas");
+    const app = new BezierApp(canvas);
+    app.step();
 
     const button = document.getElementById("generate");
-    button.onclick = () => render(gl, [canvas.clientWidth, canvas.clientHeight]);
+    button.onclick = app.step;
 });
 
-/**
- * @param {WebGL2RenderingContext} gl 
- */
-const render = async (gl, dimensions) => {
-    gl.clearColor(0.1, 0.1, 0.1, 1.0);
-    gl.clearDepth(1.0);
+class BezierApp extends App {
+    setup(gl) {
+        gl.clearColor(0.1, 0.1, 0.1, 1.0);
+        gl.clearDepth(1.0);
 
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
 
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+        gl.enable(gl.BLEND);
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-    const [vertexSource, fragmentSource] = await shaderSources;
-    const shaderProgram = createShaderProgram(gl, vertexSource, fragmentSource);
-    const attributeLocations = getAttributeLocations(gl, shaderProgram, ["position"]);
-    const uniformLocations = getUniformLocations(gl, shaderProgram, ["dimensions"]);
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    gl.useProgram(shaderProgram);
-
-    gl.uniform2fv(uniformLocations.dimensions, dimensions);
-
-    for (let i = 0; i < 8; i++) {
-        const extreme = dimensions[0] / 2;
-        const from = Vec2.randomRange(-extreme, extreme);
-        const fromCtrl = Vec2.randomRange(-extreme, extreme);
-        const toCtrl = Vec2.randomRange(-extreme, extreme);
-        const to = Vec2.randomRange(-extreme, extreme);
-
-        const segments = 32;
-        const path = [];
-        for (let i = 0; i < segments; i++) {
-            path.push([
-                ...Vec2.cubicLerp(from, fromCtrl, toCtrl, to, i / segments),
-                Math.sin(((i / (segments - 1)) - 1) * Math.PI),
-            ]);
-        }
-
-        const positions = new Float32Array(extrudeLine(path, 4).flat());
-
-        const positionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
-
-        gl.vertexAttribPointer(attributeLocations.position, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(attributeLocations.position);
-
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions.length / 3);
+        const [vertexSource, fragmentSource] = shaderSources;
+        this.shaderProgram = createShaderProgram(gl, vertexSource, fragmentSource);
+        this.attributeLocations = getAttributeLocations(gl, this.shaderProgram, ["position"]);
+        this.uniformLocations = getUniformLocations(gl, this.shaderProgram, ["dimensions"]);
     }
-};
+
+    update(_delta) {
+        this.paths = [];
+        for (let i = 0; i < 8; i++) {
+            const extreme = this.width / 2;
+            const from = Vec2.randomRange(-extreme, extreme);
+            const fromCtrl = Vec2.randomRange(-extreme, extreme);
+            const toCtrl = Vec2.randomRange(-extreme, extreme);
+            const to = Vec2.randomRange(-extreme, extreme);
+
+            const segments = 32;
+            const path = [];
+            for (let i = 0; i < segments; i++) {
+                path.push([
+                    ...Vec2.cubicLerp(from, fromCtrl, toCtrl, to, i / segments),
+                    Math.sin(((i / (segments - 1)) - 1) * Math.PI),
+                ]);
+            }
+
+            this.paths.push(path);
+        }
+    }
+
+    render(gl) {
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.useProgram(this.shaderProgram);
+        gl.uniform2fv(this.uniformLocations.dimensions, [this.width, this.height]);
+
+        for (const path of this.paths) {
+            const positions = new Float32Array(extrudeLine(path, 4).flat());
+
+            const positionBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+
+            gl.vertexAttribPointer(this.attributeLocations.position, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(this.attributeLocations.position);
+
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions.length / 3);
+        }
+    }
+}
 
 /**
  * Expects a list of 3d vectors, but mostly ignores the z component.
  */
-const extrudeLine = (points, width) => {
+function extrudeLine(points, width) {
     const pointAngles = [Vec2.angleBetween(points[0], points[1])];
     // Iterate over all but the first and last.
     for (let i = 1; i < points.length - 1; i++) {
---
+++ shared/app.js
@@ -0,0 +1,52 @@
+export class App {
+    /** @param {HTMLCanvasElement} canvas */
+    constructor(canvas) {
+        /** @type {WebGL2RenderingContext} */
+        this.gl = canvas.getContext("webgl2");
+        if (this.gl === null) throw new Error("WebGL2 not supported");
+
+        this.width = canvas.clientWidth;
+        this.height = canvas.clientHeight;
+        this.running = true;
+        this.previousTimestamp = 0;
+
+        this.setup(this.gl);
+
+        this._run = this._run.bind(this);
+        this.step = this.step.bind(this);
+    }
+
+    step(delta) {
+        this.update(delta ?? 0);
+        this.render(this.gl);
+    }
+
+    run() {
+        this.start = performance.now();
+        this.prevTimestamp = this.start;
+        window.requestAnimationFrame(this._run);
+    }
+
+    _run(timestamp) {
+        const delta = (timestamp - this.prevTimestamp) / 1000;
+        this.prevTimestamp = timestamp;
+        this.step(delta);
+        if (this.running) {
+            window.requestAnimationFrame(this._run);
+        }
+    }
+
+    /** @param {WebGL2RenderingContext} gl */
+    setup(gl) {
+        // Override in subclass
+    }
+
+    update(delta) {
+        // Override in subclass
+    }
+
+    /** @param {WebGL2RenderingContext} gl */
+    render(gl) {
+        // Override in subclass
+    }
+}
