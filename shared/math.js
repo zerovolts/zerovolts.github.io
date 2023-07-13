41b75413838dd702bc18962876c5e83c9e46f4d4
Zach Stone <zach@zerovolts.com>
2023-07-13 02:07:34 -0400

    Change const to function in math module

--- shared/math.js
+++ shared/math.js
@@ -1,27 +1,27 @@
-export const lerp = (from, to, x) => {
+export function lerp(from, to, x) {
     const span = to - from;
     return from + (span * x);
 };
 
-export const quadLerp = (from, ctrl, to, x) => {
+export function quadLerp(from, ctrl, to, x) {
     const fromLerp = lerp(from, ctrl, x);
     const toLerp = lerp(ctrl, to, x);
     return lerp(fromLerp, toLerp, x);
 }
 
-export const cubicLerp = (from, fromCtrl, toCtrl, to, x) => {
+export function cubicLerp(from, fromCtrl, toCtrl, to, x) {
     const fromLerp = quadLerp(from, fromCtrl, toCtrl, x);
     const toLerp = quadLerp(fromCtrl, toCtrl, to, x);
     return lerp(fromLerp, toLerp, x);
 }
 
-export const remap = (inMin, inMax, outMin, outMax, x) => {
+export function remap(inMin, inMax, outMin, outMax, x) {
     const inRange = inMax - inMin;
     const outRange = outMax - outMin;
     return (x * (outRange / inRange)) + (outMin - inMin);
 }
 
-export const randomRange = (min, max) => {
+export function randomRange(min, max) {
     const range = max - min;
     return Math.random() * range + min;
 }
