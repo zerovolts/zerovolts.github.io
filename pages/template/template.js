598c06d0a19dbbc4dcd18ec2e74de54c06f199a1
Zach Stone <zach@zerovolts.com>
2023-12-15 16:08:35 -0500

    Update template to use new graphics abstractions

--- pages/template/template.js
+++ pages/template/template.js
@@ -1,5 +1,5 @@
 import { GlApp } from "/shared/gl-app.js"
-import { ShaderProgram } from "/shared/graphics.js";
+import { ShaderProgram, Mesh, draw } from "/shared/graphics.js";
 
 // Initiate the fetch first to reduce perceived loading.
 let shaderSources = Promise.all([
@@ -28,49 +28,26 @@ class App extends GlApp {
             { uDimensions: "2f" },
         );
 
-        {
-            this.triangle = {
-                vertexCount: 3,
-                positionBuffer: gl.createBuffer(),
-                colorBuffer: gl.createBuffer(),
-            }
-
-            const positions = new Float32Array([
+        this.triangleMesh = new Mesh(gl, {
+            position: [
                 this.width / 2, 0,
                 this.width, this.height,
                 0, this.height,
-            ]);
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.triangle.positionBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
-
-            const colors = new Float32Array([
+            ],
+            color: [
                 1, 0, 0, 1,
                 0, 1, 0, 1,
                 0, 0, 1, 1,
-            ]);
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.triangle.colorBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
-        }
+            ],
+            index: [0, 1, 2],
+        });
     }
 
     update(_delta) {}
 
     render(gl) {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.useProgram(this.shaderProgram.program);
 
-        {
-            gl.uniform2fv(this.shaderProgram.uniforms.uDimensions.location, [this.width, this.height]);
-
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.triangle.positionBuffer);
-            gl.vertexAttribPointer(this.shaderProgram.attributes.aPosition.location, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(this.shaderProgram.attributes.aPosition.location);
-
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.triangle.colorBuffer);
-            gl.vertexAttribPointer(this.shaderProgram.attributes.aColor.location, 4, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(this.shaderProgram.attributes.aColor.location);
-
-            gl.drawArrays(gl.TRIANGLE_FAN, 0, this.triangle.vertexCount);
-        }
+        draw(gl, this.triangleMesh, this.shaderProgram, [], { uDimensions: [this.width, this.height] });
     }
 }
