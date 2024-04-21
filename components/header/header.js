9c2898abf31b9ac7ffcf0133620320abb70fff84
Zach Stone <zach@zerovolts.com>
2024-04-21 19:13:06 +0900

    Remove shadow root from header component

--- components/header/header.js
+++ components/header/header.js
@@ -4,24 +4,23 @@ const githubPath = "https://github.com/zerovolts/zerovolts.github.io/blob/main";
 
 class ZvHeader extends HTMLElement {
     async connectedCallback() {
-        this.attachShadow({ mode: "open" });
-        this.shadowRoot.innerHTML = await template;
+        this.innerHTML = await template;
 
         const title = this.getAttribute("data-title");
         if (title !== null) {
-            const titleEl = this.shadowRoot.getElementById("header-title");
+            const titleEl = document.getElementById("header-title");
             titleEl.innerText = title;
         }
 
         const relativePath = this.getAttribute("data-source");
         if (relativePath !== null) {
-            const sourceEl= this.shadowRoot.getElementById("header-source");
+            const sourceEl= document.getElementById("header-source");
             sourceEl.innerText = "Source";
             sourceEl.setAttribute("href", `${githubPath}${relativePath}`);
         }
 
         const initialTheme = localStorage.getItem("theme");
-        const themeToggleEl = this.shadowRoot.getElementById("theme-toggle");
+        const themeToggleEl = document.getElementById("theme-toggle");
         themeToggleEl.checked = initialTheme === "dark";
         themeToggleEl.addEventListener("click", e => {
             const value = e.target.checked ? "dark" : "light";
@@ -32,7 +31,7 @@ class ZvHeader extends HTMLElement {
                 : "/assets/images/zakku.png";
         });
 
-        const siteLogoEl = this.shadowRoot.getElementById("site-logo");
+        const siteLogoEl = document.getElementById("site-logo");
         if (localStorage.getItem("theme") === "dark") {
             siteLogoEl.src = "/assets/images/zakku-dark.png";
         }
