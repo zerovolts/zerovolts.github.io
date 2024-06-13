a343184a2db6193a5cd5f4ca2bff1e740ba5550f
Zach Stone <zach@zerovolts.com>
2024-06-13 05:33:14 -0400

    Add Mat4 toString method

--- shared/mat4.js
+++ shared/mat4.js
@@ -160,4 +160,13 @@ export class Mat4 {
     project(fov, aspect, near, far) {
         return this.mul(Mat4.projection(fov, aspect, near, far));
     }
+
+    toString() {
+        return [
+            `[${this.data[0 ]}, ${this.data[1 ]}, ${this.data[2 ]}, ${this.data[3 ]}]`,
+            `[${this.data[4 ]}, ${this.data[5 ]}, ${this.data[6 ]}, ${this.data[7 ]}]`,
+            `[${this.data[8 ]}, ${this.data[9 ]}, ${this.data[10]}, ${this.data[11]}]`,
+            `[${this.data[12]}, ${this.data[13]}, ${this.data[14]}, ${this.data[15]}]`,
+        ].join("\n");
+    }
 }
